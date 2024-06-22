d03dc49515b2f8d3988a66d0bf564e2dc9fa7cfa
JoseMurillo2711 <josemurillo2711@gmail.com>
2024-06-22 12:20:29 -0500

    UPD: CircularDoubleNodeList implementation

--- ProyectoEDD1/src/proyectoedd1/List.java
+++ ProyectoEDD1/src/proyectoedd1/List.java
@@ -4,10 +4,28 @@
  */
 package proyectoedd1;
 
+
 /**
  *
- * @author richardo
+ * @author josem
+ * @param <E>
  */
-public interface List {
+public interface List<E> extends Iterable<E> {
+    
+    public int size();
+    public  boolean isEmpty();
+    public void clear();
+    public boolean addFirst(E e); 
+    public void addLast(E e);
+    public E removeFirst();
+    public E removeLast(); 
+    public void add (int index, E element);
+    public E remove (int index); 
+    public E get (int index);
+    public E set (int index, E element);
+    @Override
+    public String toString();
+    public List<E> findIntersection(List<E> otherList);
+
     
 }

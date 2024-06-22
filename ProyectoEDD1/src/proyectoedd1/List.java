7a4d753a7e82c01ee4502865b2bfd4783f930c11
JoseMurillo2711 <josemurillo2711@gmail.com>
2024-06-22 09:35:07 -0500

    list interface

---
+++ ProyectoEDD1/src/proyectoedd1/List.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package proyectoedd1;
+
+/**
+ *
+ * @author richardo
+ */
+public interface List {
+    
+}

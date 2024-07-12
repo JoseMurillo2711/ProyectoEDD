bd61acd40d8930a2b452617aba0b9be1f180820c
Michelle Arreaga <miaralva@espol.edu.ec>
2024-07-12 05:24:58 -0500

    Se movio la clase FiltrarVehiculo a paquete util
    FiltrarVehiculo no es parte del modelo del programa

---
+++ ProyectoEDD1/src/util/FiltrarVehiculo.java
@@ -0,0 +1,37 @@
+package util;
+
+import TDA.DoubleCircleLinkedList;
+import modelo.Vehiculo;
+
+public class FiltrarVehiculo {
+
+    public DoubleCircleLinkedList<Vehiculo> filtrarPorMarcaYModelo(DoubleCircleLinkedList<Vehiculo> vehiculos, String marca, String modelo) {
+        DoubleCircleLinkedList<Vehiculo> resultado = new DoubleCircleLinkedList<>();
+        for (Vehiculo v : vehiculos) {
+            if (v.getMarca().equalsIgnoreCase(marca) && v.getModelo().equalsIgnoreCase(modelo)) {
+                resultado.addLast(v);
+            }
+        }
+        return resultado;
+    }
+
+    public DoubleCircleLinkedList<Vehiculo> filtrarPorRangoPrecio(DoubleCircleLinkedList<Vehiculo> vehiculos, double precioMin, double precioMax) {
+        DoubleCircleLinkedList<Vehiculo> resultado = new DoubleCircleLinkedList<>();
+        for (Vehiculo v : vehiculos) {
+            if (v.getPrecio() >= precioMin && v.getPrecio() <= precioMax) {
+                resultado.addLast(v);
+            }
+        }
+        return resultado;
+    }
+
+    public DoubleCircleLinkedList<Vehiculo> filtrarPorRangoKilometraje(DoubleCircleLinkedList<Vehiculo> vehiculos, int kmMin, int kmMax) {
+        DoubleCircleLinkedList<Vehiculo> resultado = new DoubleCircleLinkedList<>();
+        for (Vehiculo v : vehiculos) {
+            if (v.getKilometraje() >= kmMin && v.getKilometraje() <= kmMax) {
+                resultado.addLast(v);
+            }
+        }
+        return resultado;
+    }
+}
